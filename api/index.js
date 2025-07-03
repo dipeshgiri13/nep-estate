9183020ac485d8f2b0c30713b2e539c0143b6f3d
Dipesh Giri <deepeshgiree13@gmail.com>
2025-07-04 01:07:25 +0545

    Connect to Database 2

--- api/index.js
+++ api/index.js
@@ -1,8 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("✅ Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
 const app = express();
 
-console.log("✅ App ");
+console.log("✅ App hello  ");
 
-app.listen(3001, () => {
-  console.log("Server is running on port 3001");
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
