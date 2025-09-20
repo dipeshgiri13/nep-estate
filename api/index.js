8b25eb33c030c6bb44e3bdda2edb5f2dbc742899
Dipesh Giri <deepeshgiree13@gmail.com>
2025-09-20 09:43:19 +0545

    Add Pre Installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
+
 dotenv.config();
 
 // Connect to MongoDB
@@ -16,6 +18,8 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -31,6 +35,12 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 //Creating a middleware to handle error
 app.use((err, req, res, next) => {
