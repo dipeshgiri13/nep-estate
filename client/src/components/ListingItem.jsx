d80e949201fcb99acb5510518a6a027a3af68d47
Dipesh Giri <deepeshgiree13@gmail.com>
2025-10-29 18:49:48 +0545

    Correction Nrs.

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -27,7 +27,6 @@ export default function ListingItem({ listing }) {
             {listing.description}
           </p>
           <p className="text-slate-500 mt-2 font-semibold ">
-            $
             {listing.offer
               ? listing.discountPrice.toLocaleString("en-US")
               : listing.regularPrice.toLocaleString("en-US")}
